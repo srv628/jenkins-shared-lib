6e6a8a687d073312d00e580e95a190cfff561953
Sourabh Nagve <sourabhnagve28@gmail.com>
2025-07-21 12:52:47 +0530

    added the two files buildImage and buildjar

---
+++ var/buildImage.groovy
@@ -0,0 +1,12 @@
+#!/bin/env groovy
+def call(){
+    echo "building the docker image"
+    withCredentials([usernamePassword(credentialsId:"dockerhub-cred",usernameVariable:"USER",passwordVariable:"PASS")]){
+        sh "docker build -t srvwin/dockerinitial:javamapp-2.0 ."
+
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+
+
+        sh "docker push  srvwin/dockerinitial:javamapp-2.0"
+    }
+    }
---
+++ var/buildjar.groovy
@@ -0,0 +1,5 @@
+#!/bin/env groovy
+def call(){
+    echo "building the jar file"
+    sh "mvn package"
+}
